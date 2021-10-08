e15cdb4f242b1f684d00646561bcfceccaebd255
alyssanotfound <davia317@gmail.com>
2021-10-08 17:14:52 -0400

    add this dialogue

---
+++ public/js/this-dialogue-vid_array.js
@@ -0,0 +1,11 @@
+var textInfo = [  
+"<div>Ivan Cheng, <i>Changing Room 4: EX-CURSES [Ouija DM]</i>, <div class='number'>2020</div>, HD video, 30 min 03 sec.</div>",
+"<div>Alli Melanson, <i>Ultimate Escape Artist</i>, <div class='number'>2021</div>, Digital video, 2 min 37 sec.</div>",
+"<div>Alli Melanson, <i>The Real Trick (ft. Lode, ft. Alyssa Davis Gallery)</i>, <div class='number'>2021</div>, Digital video, 2 min 51 sec.</div>",
+"<div>Vijay Masharani, <i>Command | Plea</i>, <div class='number'>2020</div>, Single channel video, r/t, 4 min.</div>",
+ ];
+
+
+
+
+
---
+++ public/js/this-dialogue_array.js
@@ -0,0 +1,29 @@
+var textInfo = [  
+"<div>Installation view: <i>This dialogue which happened to be present in all previous dialogues</i>, Alyssa Davis Gallery, <div class='number'>2021</div>.</div>",
+"<div>Installation view: <i>This dialogue which happened to be present in all previous dialogues</i>, Alyssa Davis Gallery, <div class='number'>2021</div>.</div>",
+"<div>Ivan Cheng, <i>Changing Room 4: EX-CURSES [Ouija DM]</i>, <div class='number'>2020</div>, HD video, 30 min 03 sec.</div>",
+"<div>Ivan Cheng, <i>Changing Room 4: EX-CURSES [Ouija DM]</i>, <div class='number'>2020</div>, HD video, 30 min 03 sec.</div>",
+"<div>Installation view: <i>This dialogue which happened to be present in all previous dialogues</i>, Alyssa Davis Gallery, <div class='number'>2021</div>.</div>",
+"<div>Libby Rothfeld, <i>Word Search on the Elliptical (The Scoreboard)</i>, <div class='number'>2020</div>, Laminate, digital print, wood, 17 5/8 x 23 7/8 x 3 in. (44.77 x 60.64 x 7.62 cm)</div>",
+"<div>Installation view: <i>This dialogue which happened to be present in all previous dialogues</i>, Alyssa Davis Gallery, <div class='number'>2021</div>.</div>",
+"<div>Alli Melanson, <i>Ultimate Escape Artist</i>, <div class='number'>2021</div>, Digital video, 2 min 37 sec.</div>",
+"<div>Alli Melanson, <i>The Real Trick (ft. Lode, ft. Alyssa Davis Gallery)</i>, <div class='number'>2021</div>, Digital video, 2 min 51 sec.</div>",
+"<div>Installation view: <i>This dialogue which happened to be present in all previous dialogues</i>, Alyssa Davis Gallery, <div class='number'>2021</div>.</div>",
+"<div>Libby Rothfeld, <i>In the Computer Room (Lamps, Seabiscuit DVD, and Football Players)</i>, <div class='number'>2020</div>, Laminate, digital print, wood, 23 7/8 x 17 5/8 x 3 in. (60.64 x 44.77 x 7.62 cm)</div>",
+"<div>Installation view: <i>This dialogue which happened to be present in all previous dialogues</i>, Alyssa Davis Gallery, <div class='number'>2021</div>.</div>",
+"<div>Vijay Masharani, <i>Command | Plea</i>, <div class='number'>2020</div>, Single channel video, r/t, 4 min.</div>",
+"<div>sgp, <i>princex blank or: how I learned to love, 1.8 milliseconds at a time</i>, <div class='number'>2021</div>, Found wax, solar charging panel, hardware, software, wetware, borrowed iPad, video, Dimensions variable</div>",
+"<div>sgp, <i>princex blank or: how I learned to love, 1.8 milliseconds at a time</i>, <div class='number'>2021</div>, detail</div>",
+"<div>sgp, <i>princex blank or: how I learned to love, 1.8 milliseconds at a time</i>, <div class='number'>2021</div>, detail</div>",
+"<div>sgp, <i>princex blank or: how I learned to love, 1.8 milliseconds at a time</i>, <div class='number'>2021</div>, detail</div>",
+"<div>sgp, <i>princex blank or: how I learned to love, 1.8 milliseconds at a time</i>, <div class='number'>2021</div>, detail</div>",
+"<div>sgp, <i>princex blank or: how I learned to love, 1.8 milliseconds at a time</i>, <div class='number'>2021</div>, detail</div>",
+"<div>Installation view: <i>This dialogue which happened to be present in all previous dialogues</i>, Alyssa Davis Gallery, <div class='number'>2021</div>.</div>",
+"<div>Installation view: <i>This dialogue which happened to be present in all previous dialogues</i>, Alyssa Davis Gallery, <div class='number'>2021</div>.</div>",
+"<div>Libby Rothfeld, <i>The Stairs</i>, <div class='number'>2020</div>, Laminate, digital print, wood, 23 7/8 x 17 5/8 x 3 in. (60.64 x 44.77 x 7.62 cm)</div>",
+ ];
+
+
+
+
+
