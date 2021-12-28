e156fc3d1892ac0ab54d11b9c4c182ce3ac7d9be
alyssanotfound <davia317@gmail.com>
2021-12-28 15:06:32 -0500

    add abbys show

---
+++ public/js/abby-lloyd_array.js
@@ -0,0 +1,14 @@
+var textInfo = [  
+"<div>Abby Lloyd, Installation view: <i>Goodbye Dolly</i>, Alyssa Davis Gallery, <div class='number'>2021</div></div>",
+"<div>Abby Lloyd, Installation view: <i>Goodbye Dolly</i>, Alyssa Davis Gallery, <div class='number'>2021</div></div>",
+"<div><i>Goodbye Dolly</i>, <div class='number'>2021</div>, Fabric, poly-fil, batting, foam, packing peanuts, plaster, vinyl, spray paint, 98 x 148 x 160 in.</div>",
+"<div><i>Goodbye Dolly</i>, <div class='number'>2021</div>, detail</div>",
+"<div><i>Goodbye Dolly</i>, <div class='number'>2021</div>, detail</div>",
+"<div><i>Goodbye Dolly</i>, <div class='number'>2021</div>, detail</div>",
+"<div><i>Goodbye Dolly</i>, <div class='number'>2021</div>, detail</div>"
+ ];
+
+
+
+
+
