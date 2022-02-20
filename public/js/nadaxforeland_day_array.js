0ad53892ca85ce39767c877237d87d5e36fdba80
alyssanotfound <davia317@gmail.com>
2022-02-20 00:12:26 -0500

    add nada foreland

---
+++ public/js/nadaxforeland_day_array.js
@@ -0,0 +1,8 @@
+var textInfo = [  "<div>Genevieve Goffman, <i>The Assassination of Winston Church Mouse</i>, <div class='number'>2021</div>, PLA, plexiglass, silicone, Citadel acrylic paint, clear enamel, resin, <div class='number'>33</div> x <div class='number'>27</div> x <div class='number'>70</div> in.</div>",
+"<div><i>The Assassination of Winston Church Mouse</i>, <div class='number'>2021</div>, detail</div>",
+"<div><i>The Assassination of Winston Church Mouse</i>, <div class='number'>2021</div>, detail</div>",
+"<div><i>The Assassination of Winston Church Mouse</i>, <div class='number'>2021</div>, detail</div>",
+"<div><i>The Assassination of Winston Church Mouse</i>, <div class='number'>2021</div>, detail</div>",
+"<div><i>The Assassination of Winston Church Mouse</i>, <div class='number'>2021</div>, detail</div>",
+"<div>Genevieve Goffman, Installation view: NADA x Foreland 2021</div>",
+ ];
